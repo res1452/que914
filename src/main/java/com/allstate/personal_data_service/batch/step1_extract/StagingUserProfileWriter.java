016c5a7ec9bd46d9d3c1cd4ee01e624f15d07cda
res1452 <daniel.alemayehu154@gmail.com>
2025-07-08 11:01:06 -0500

    Added a rest controller

---
+++ src/main/java/com/allstate/personal_data_service/batch/step1_extract/StagingUserProfileWriter.java
@@ -0,0 +1,4 @@
+package com.allstate.personal_data_service.batch.step1_extract;
+
+public class StagingUserProfileWriter {
+}
---
+++ src/main/java/com/allstate/personal_data_service/controller/UserProfileRestController.java
@@ -0,0 +1,4 @@
+package com.allstate.personal_data_service.controller;
+
+public class UserProfileRestController {
+}
