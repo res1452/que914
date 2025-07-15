7fd18675e8f7828b28e2072768c27844c81c8ae0
res1452 <daniel.alemayehu154@gmail.com>
2025-07-15 12:14:40 -0500

    Added a user response dto

--- src/main/java/com/allstate/personal_data_service/dto/UserProfileResponse.java
+++ src/main/java/com/allstate/personal_data_service/dto/UserProfileResponse.java
@@ -1,7 +1,33 @@
 package com.allstate.personal_data_service.dto;
 
+import com.allstate.personal_data_service.model.Address;
+import com.allstate.personal_data_service.model.Vehicle;
 import lombok.Data;
+
+import java.time.LocalDateTime;
 
 @Data
 public class UserProfileResponse {
+
+    private Long id;
+
+    private String firstName;
+    private String lastName;
+    private String fullName;
+
+    private String email;
+    private String phoneNumber;
+
+    private String dateOfBirth;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+    private String gender;
+    private String maritalStatus;
+    private String role;
+    private String locale;
+    private boolean wantsPromotions;
+
+    private Address address;
+    private Vehicle vehicle;
 }
